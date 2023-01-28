29a9b350a61bb2120e4c6074d57357be01a2405e
Salako Oluwadolapo <willdynamics@gmail.com>
2023-01-28 16:17:33 +0100

    Update deepinsight.py

--- deepinsight.py
+++ deepinsight.py
@@ -230,7 +230,7 @@ for seconds in range(200):
             st.write(candidate_word_cloud(keyword1=selected))
         #st.markdown("### Detailed Data View")
         st.dataframe(sentiment_analysis(keyword1=selected)[['Raw tweet', 'compound']].tail())
-        time.sleep(60)
         file_name_recent = str.lower(selected) + ".csv"
         st.download_button(label="Download data as CSV", data=csv, file_name=file_name_recent, mime='text/csv')
+        time.sleep(60)
 #extract_tweets("Kwankwaso", "IsaacIdahosa", from_date="2023-01-28", number_of_tweets_to_retrieve=10)  
